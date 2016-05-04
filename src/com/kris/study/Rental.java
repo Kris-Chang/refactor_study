adba6c81e7674daab78de7aacbd84ce4259917d2
Kris-Chang <zhangqiminhd@126.com>
2016-05-04 19:28:52 +0800

    demo

---
+++ src/com/kris/study/Rental.java
@@ -0,0 +1,23 @@
+package com.kris.study;
+
+/**
+ * Created by Kris on 16/5/4.
+ */
+public class Rental {
+
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie _movie, int _daysRented) {
+        this._movie = _movie;
+        this._daysRented = _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+}
